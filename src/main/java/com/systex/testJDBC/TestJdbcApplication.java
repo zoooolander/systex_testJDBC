89eb3e4537e647c89f8974ed2dcc70852c712859
zoooolander <christina8318@gmail.com>
2024-09-25 17:58:10 +0800

    20240925 course

--- src/main/java/com/systex/testJDBC/TestJdbcApplication.java
+++ src/main/java/com/systex/testJDBC/TestJdbcApplication.java
@@ -7,12 +7,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.systex.testJDBC.lab3.Product;
 import com.systex.testJDBC.lab3.ProductDao;
+import com.systex.testJDBC.lab3.TxProductService;
 
 @SpringBootApplication
 public class TestJdbcApplication implements CommandLineRunner {
 
 	@Autowired
 	private ProductDao productDao;
+	
+	@Autowired
+	private TxProductService txProductService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestJdbcApplication.class, args);
@@ -21,22 +25,23 @@ public class TestJdbcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		Product product = new Product();
-		product.setName("lingonberry juice");
-		product.setDescription("drinks");
-		System.out.println(this.productDao.insert(product));
-		this.productDao.getProducts().forEach(p -> System.out.println(p));
-		System.out.println("==================================================");
-		System.out.println(productDao.getProduct(1));
-		System.out.println(productDao.getProduct(2));
-		Product p1 = new Product();
-		p1.setProductId(1);
-		p1.setName("Monkey");
-		p1.setDescription("this is a monkey");
-
-		productDao.update(p1);
-		System.out.println("==================================================");
-		this.productDao.getProducts().forEach(p -> System.out.println(p));
+//		Product product = new Product();
+//		product.setName("lingonberry juice");
+//		product.setDescription("drinks");
+//		System.out.println(this.productDao.insert(product));
+//		this.productDao.getProducts().forEach(p -> System.out.println(p));
+//		System.out.println("==================================================");
+//		System.out.println(productDao.getProduct(1));
+//		System.out.println(productDao.getProduct(2));
+//		Product p1 = new Product();
+//		p1.setProductId(1);
+//		p1.setName("Monkey");
+//		p1.setDescription("this is a monkey");
+//
+//		productDao.update(p1);
+//		System.out.println("==================================================");
+//		this.productDao.getProducts().forEach(p -> System.out.println(p));
+		txProductService.testTx();
 	}
 
 }
---
+++ src/main/java/com/systex/testJDBC/lab3/ALittleSeriousException.java
@@ -0,0 +1,8 @@
+package com.systex.testJDBC.lab3;
+
+public class ALittleSeriousException extends Exception {
+
+	public ALittleSeriousException(String message) {
+		super(message);
+	}
+}
